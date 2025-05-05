a5c5854dd281d388def10916dca296e14c3ba351
Gary Woodfine <lnb0l9dc@duck.com>
2025-05-05 21:18:07 +0100

    Add app config, manifest, and favicon assets
    Introduced a configuration file to define UI settings with primary and neutral colors. Added a web manifest and multiple favicon

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <UPageHero
@@ -9,6 +10,23 @@ technical expertise, insatiable curiosity, and analytical mindset to solve compl
 business problems "
       headline="Back-end software engineer"
   />
+  <UPageMarquee>
+    <UIcon name="mdi-language-csharp" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-dotnet" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="fa6-brands-golang" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-rust" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-javascript" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-typescript" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi-vuejs" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="simple-icons-nuxt" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi-aws" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi-microsoft-azure" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-digitalocean" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-mongodb" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="lineicons-postgresql" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-microsoftsqlserver" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="codicon-terminal-linux" class="size-16 shrink-0 text-orange-500" />
+  </UPageMarquee>
 </template>
 
 <style scoped></style>
