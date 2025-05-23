2adc240b36c6363c15a4418523ed36a0b3660cd0
Gary Woodfine <lnb0l9dc@duck.com>
2025-05-23 14:42:19 +0100

    Remove `pnpm-lock.yaml`
    Deleted the lockfile to reset dependencies or due to migration/cleanup.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,12 +5,17 @@
   <UPageHero
       title="Gary Woodfine"
       description="Random thoughts and musings on the world of Economics, Politics and Technology"
-      headline="Opinionated Software Developer"
+      headline="The other Gary's economics"
   />
   <UPageMarquee>
     <UIcon name="codicon-terminal-linux" class="size-16 shrink-0 text-orange-500" />
     <UIcon name="uil-bitcoin" class="size-16 shrink-0 text-orange-500" />
     <UIcon name="i-simple-icons-litecoin" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="material-symbols-currency-pound" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="bi-currency-dollar" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="streamline-decent-work-and-economic-growth" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="game-icons-anarchy" class="size-16 shrink-0 text-orange-500" />
+    material-symbols:currency-pound
   </UPageMarquee>
 </template>
 
