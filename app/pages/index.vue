26865222fdcb11880ddc0f68e10f816b840faf76
Gary Woodfine <lnb0l9dc@duck.com>
2025-05-23 14:10:11 +0100

    poo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,24 +4,10 @@
 <template>
   <UPageHero
       title="Gary Woodfine"
-      description="Random thoughts and musings regarding the world of technology"
-      headline="Opinionated Software Development"
+      description="Random thoughts and musings on the world of Economics, Politics and Technology"
+      headline="Opinionated Software Developer"
   />
   <UPageMarquee>
-    <UIcon name="mdi-language-csharp" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-dotnet" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="fa6-brands-golang" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-rust" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-javascript" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-typescript" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi-vuejs" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="simple-icons-nuxt" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi-aws" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi-microsoft-azure" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-digitalocean" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-mongodb" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="lineicons-postgresql" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-microsoftsqlserver" class="size-16 shrink-0 text-orange-500" />
     <UIcon name="codicon-terminal-linux" class="size-16 shrink-0 text-orange-500" />
     <UIcon name="uil-bitcoin" class="size-16 shrink-0 text-orange-500" />
     <UIcon name="i-simple-icons-litecoin" class="size-16 shrink-0 text-orange-500" />
