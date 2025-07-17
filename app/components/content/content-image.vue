21e7bfefff4f434c20caec2f00477ff5275fc98d
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-17 08:54:57 +0100

    Update `content-image.vue` styles to improve image rendering and layout
    - Adjusted image classes for better object-fit and resizing.
    - Added rounded corners and margin adjustments for enhanced design consistency.
    - Removed unused scoped `.image-style` CSS class.

--- app/components/content/content-image.vue
+++ app/components/content/content-image.vue
@@ -16,28 +16,26 @@ defineProps({
 </script>
 
 <template>
-  <div class="container mx-auto pb-10">
-    <div v-if="link !== ''" class="max-w-screen-lg flex justify-center mx-auto">
+  <div class="container mx-auto pb-10 mt-5">
+    <div v-if="link !== ''" class="max-w-screen-lg flex justify-center mx-auto rounded-2xl">
       <a :href="link" target="_blank">
         <nuxt-img
             :src="image"
             :title="title"
-            class="image-style"
+            class="object-cover resize w-full rounded-2xl"
         />
       </a>
     </div>
-    <div v-else-if="link ===''" class="max-w-screen-lg flex justify-center mx-auto">
+    <div v-else-if="link ===''" class="max-w-screen-lg flex justify-center mx-auto rounded-2xl">
       <nuxt-img
           :src="image"
           :title="title"
-          class="image-style"
+          class="object-cover resize w-full rounded-2xl"
       />
     </div>
   </div>
 </template>
 
 <style scoped>
-.image-style {
-  @apply object-cover resize w-full
-}
+
 </style>
