ff05a8d6c4710abf17587b933842522afd982381
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-18 22:05:55 +0100

    Add `category` field to post schema and enhance `post-link.vue` with date formatting and category badge

--- app/components/content/post-link.vue
+++ app/components/content/post-link.vue
@@ -26,20 +26,23 @@ const formatDate = (dateString: string) => {
 </script>
 
 <template>
-  <div v-if="post" class="max-w-4xl px-10 my-4 py-6 rounded-lg shadow-md dark:shadow-orange-50">
-    <div class="flex justify-between items-center mb-3">
-      <span class="font-light text-orange-300">{{formatDate(post.date)}}</span>
-      <u-badge color="primary" :label="post.category" size="sm"></u-badge>
-    </div>
-    <a :href="post.path" class="text-orange-500 hover:underline">{{post?.title}}</a>
-    <div class="mt-2"> <p  class="text-sm">{{post?.summary}}</p></div>
-    <div class="flex justify-between items-center mt-4">
-      <a class="text-orange-500 hover:underline text-sm" :href="post.path">Read more</a>
-      <div>
-        <a class="flex items-center" :href="post.path">
-          <nuxt-img class="mx-4 w-6 h-6 object-cover rounded-full hidden sm:block" :src="post?.author?.avatar?.src" alt="avatar" />
-          <p class="text-orange-500 font-bold text-sm">{{post.author.name}}</p>
-        </a>
+  <div class="container mx-auto pb-3 mt-5 flex justify-center">
+    <div v-if="post" class="max-w-4xl px-10 my-4 py-6 rounded-lg shadow-md dark:shadow-orange-50 ">
+      <div class="flex justify-between items-center mb-3">
+        <span class="font-light text-orange-300">{{ formatDate(post.date) }}</span>
+        <u-badge color="primary" :label="post.category" size="sm"></u-badge>
+      </div>
+      <a :href="post.path" class="text-orange-500 hover:underline">{{ post?.title }}</a>
+      <div class="mt-2"><p class="text-sm">{{ post?.summary }}</p></div>
+      <div class="flex justify-between items-center mt-4">
+        <a class="text-orange-500 hover:underline text-sm" :href="post.path">Read more</a>
+        <div>
+          <a class="flex items-center" :href="post.path">
+            <nuxt-img class="mx-4 w-6 h-6 object-cover rounded-full hidden sm:block" :src="post?.author?.avatar?.src"
+                      alt="avatar"/>
+            <p class="text-orange-500 font-bold text-sm">{{ post.author.name }}</p>
+          </a>
+        </div>
       </div>
     </div>
   </div>
