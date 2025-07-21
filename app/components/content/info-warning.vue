52b92273d02763b04d243a6cd4db78c8b88abce1
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-21 23:49:30 +0100

    Tweak `info-warning.vue` background color for better contrast

--- app/components/content/info-warning.vue
+++ app/components/content/info-warning.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 </script>
 <template>
-  <div class="bg-yellow-100 rounded-md shadow-lg text-orange-500 px-4 py-3" role="alert">
+  <div class="bg-yellow-50 rounded-md shadow-lg text-orange-500 px-4 py-3" role="alert">
     <p class="font-bold"><Icon name="carbon:warning-alt" class="h-8 w-8 mr-3"/></p>
     <div class="text-md pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
   </div>
