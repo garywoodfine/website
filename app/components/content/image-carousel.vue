83d7a0dbe721d3a840143ead905032ab0136d0a8
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-23 22:01:28 +0100

    Add `image-carousel.vue` component for image sliding functionality and update `quote-text.vue` formatting.

---
+++ app/components/content/image-carousel.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  images: {
+    type: Array as () => string[],
+    required: true
+  },
+
+})
+
+console.log(props.images)
+</script>
+
+<template>
+  <UCarousel
+      v-slot="{ item: image }"
+      arrows
+      dots
+      :ui="{ item: 'basis-1/2' }"
+      :items="images"
+      :prev="{ color: 'primary', variant: 'solid' }"
+      :next="{ color: 'primary', variant: 'solid' }"
+
+  >
+    <NuxtImg :src="image" width="900" height="900" class="rounded-lg" />
+  </UCarousel>
+</template>
+
+<style scoped></style>
--- app/components/content/quote-text.vue
+++ app/components/content/quote-text.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
 </script>
-
 <template>
-
   <div class="w-full mx-auto rounded-lg bg-gray-100 shadow-lg px-5 pt-5 pb-10 text-gray-900" >
     <div class="w-full mb-10">
       <div class="text-4xl text-orange-500 text-left h-6"><icon name="bx:bxs-quote-alt-left"/></div>
@@ -11,7 +9,5 @@
       <div class="text-xs pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="citation"/></div>
     </div>
   </div>
-
 </template>
-
 <style scoped></style>
