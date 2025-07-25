d11b6c63542a15d9b9c6f9f69276a1bae7c85510
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-25 08:44:23 +0100

    Update `about.vue` content to include detailed personal and professional biography

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,8 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div>
-    Page: about
+    <h1>About Me</h1>
+    <p>Hi, I'm Gary Woodfine, I'm an independent software developer, architect, consultant, blogger, Bitcoin & Nostr Enthusiast.
+      I've provided software development expertise and helped augment software development skills and teams for a number of
+      startups, product companies, corporate IT shops, and consulting firms.</p>
+
+    <p>I started out in the software world back in 1995, building websites with Hot Metal Pro and ASP and moved on to
+      building web apps with .NET when it released. Since then, I've shipped systems in just about every industry.</p>
+
+    <p>Over the years, I've strived to improve how I and my teams deliver value to the clients, customers, and end-users.
+      That has led me down the path of exploring Agile, Domain-Driven Design, REST, messaging, distributed systems,
+      and anything else I thought could help.</p>
   </div>
 </template>
 
