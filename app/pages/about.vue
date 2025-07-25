5d9ae4c309825ba7cfa91883dec7fd83be8b4b7b
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-25 08:55:54 +0100

    Update `about.vue` with expanded biography, highlighting extensive professional experience and technical versatility

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -8,8 +8,12 @@
       I've provided software development expertise and helped augment software development skills and teams for a number of
       startups, product companies, corporate IT shops, and consulting firms.</p>
 
-    <p>I started out in the software world back in 1995, building websites with Hot Metal Pro and ASP and moved on to
-      building web apps with .NET when it released. Since then, I've shipped systems in just about every industry.</p>
+    <p>I started out in the software world back in 1995, building websites with HoTMetaL Pro & ASP and moved on to
+      building web apps with .NET when it released in Beta in 2001, since then, I've shipped systems in just about every industry.</p>
+    <p>Along the path, I have also shipped various platforms & products in Java, PHP, Javascript, Go, Rust, Python & just about
+    any lanaguage or software development framework that has had its moment in the sun, but somehow I have always made way
+    back to .Net.</p>
+
 
     <p>Over the years, I've strived to improve how I and my teams deliver value to the clients, customers, and end-users.
       That has led me down the path of exploring Agile, Domain-Driven Design, REST, messaging, distributed systems,
