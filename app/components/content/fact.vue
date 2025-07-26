88621f7d22927e74901761ef4d4875e2768fd257
Gary Woodfine <gary.woodfine@threenine.co.uk>
2025-07-26 23:37:35 +0100

    Adjust typography components to use `flex` for better alignment of icons and text.

--- app/components/content/fact.vue
+++ app/components/content/fact.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <div class="bg-blue-50 rounded-md shadow-lg text-blue-600 px-4 py-3" role="alert">
-    <p class="font-bold"><Icon name="material-symbols:fact-check-outline" class="h-8 w-8 mr-3" />Fact</p>
+    <p class="font-bold flex items-center"><Icon name="material-symbols:fact-check-outline" class="h-8 w-8 mr-3" />Fact</p>
     <div class="text-md pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
   </div>
 </template>
