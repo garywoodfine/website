9fc574aaf60b3c86359636105a62f18b16d08e01
Gary Woodfine <gary.woodfine@threenine.co.uk>
2025-07-27 18:06:20 +0100

    Enhance typography styles and improve alignment in `info-warning` component.

--- app/components/content/info-warning.vue
+++ app/components/content/info-warning.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <div class="bg-yellow-50 rounded-md shadow-lg text-orange-500 px-4 py-3" role="alert">
-    <p class="font-bold"><Icon name="carbon:warning-alt" class="h-8 w-8 mr-3"/></p>
+    <p class="font-bold flex items-center"><Icon name="carbon:warning-alt" class="h-8 w-8 mr-3"/><slot name="title"/></p>
     <div class="text-md pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
   </div>
 </template>
