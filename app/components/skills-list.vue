2536bd9fd4eac3d1e45c9f830dc823d5031d7a56
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-30 09:34:11 +0100

    Define `Skill` type and use it in `skills-list` component.

--- app/components/skills-list.vue
+++ app/components/skills-list.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import type { Skill } from '~/types/skill'
+
 defineProps({
   category: {
     type: String,
     required: true,
   },
   technologies: {
-    type: Array,
+    type: Array as () => Skill[],
     default: () => [
       { name: 'C#', icon:'mdi:slack', level: 'Advanced' },
       { name: 'C#', icon:'mdi:slack', level: 'Advanced' },
@@ -22,7 +24,7 @@ defineProps({
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div v-for="(skill, index) in technologies" :key="index" class="p-4 border border-orange-100 rounded-lg shadow-sm">
         <span class="font-medium text-xl flex items-center gap-2"><icon :name="skill.icon" class="text-orange-500 mr-1"  size="45" />{{skill.name}}</span>
-        <p class="text-sm text-gray-600 mt-3">{{ skill.level }}</p>
+        <p  class="text-sm text-gray-600 mt-3">{{ skill.level }}</p>
       </div>
       </div>
     </div>
