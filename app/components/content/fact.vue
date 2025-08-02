1ddc264e3207034cc589b3abae9514361a24f069
Gary Woodfine <lnb0l9dc@duck.com>
2025-08-02 16:09:06 +0100

    Update text color in `fact.vue` from `text-blue-700` to `text-blue-800` for improved readability.

--- app/components/content/fact.vue
+++ app/components/content/fact.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 </script>
 <template>
-  <div class="bg-blue-50 rounded-md shadow-lg text-blue-700 px-4 py-3" role="alert">
+  <div class="bg-blue-50 rounded-md shadow-lg text-blue-800 px-4 py-3" role="alert">
     <p class="font-bold flex items-center"><Icon name="material-symbols:fact-check-outline" class="h-8 w-8 mr-3" />Fact</p>
     <div class="text-md pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
   </div>
