28faacee69364cbb84f26ff3a2937d235edc7a0a
Gary Woodfine <lnb0l9dc@duck.com>
2025-08-06 14:36:20 +0100

    Update `nuxt` to version `^4.0.3` and add new redirect for "Building Microservices, 2nd Edition".

--- app/components/content/notice.vue
+++ app/components/content/notice.vue
@@ -1,10 +1,7 @@
 <script lang="ts" setup></script>
-
 <template>
   <div class="bg-gray-200 border border-l-orange-500 border-l-4 rounded-md shadow-lg text-gary-800 px-4 py-3" role="alert">
     <span class="font-bold flex items-center"><Icon name="pepicons-pop:bulletin-notice" class="h-8 w-8 mr-3 text-orange-500" /></span>
     <div class="text-md pl-2 pr-2 text-orange-500 text-2xl"><slot name="message" /></div>
   </div>
 </template>
-
-<style scoped></style>
