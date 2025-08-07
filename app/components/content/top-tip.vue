045d7a59ebab3ceebdc0c6651985c241e3b055bd
Gary Woodfine <lnb0l9dc@duck.com>
2025-08-07 21:25:45 +0100

    Adjust text size in `top-tip.vue` from `text-md` to `text-xl` for improved readability.

--- app/components/content/top-tip.vue
+++ app/components/content/top-tip.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="bg-blue-100 rounded-md shadow-lg text-blue-700 px-4 py-3 mb-5 mt-3" role="alert">
     <span class="font-bold flex items-center"><Icon name="ic:sharp-tips-and-updates" class="h-8 w-8 mr-3" />Top Tip</span>
-    <div class="text-md pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
+    <div class="text-xl pl-2 pr-2 mt-2 whitespace-pre-line"><slot name="message" /></div>
   </div>
 </template>
 <style scoped></style>
