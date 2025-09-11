3e6a24e62e7a767774196ccfc4c67aabfb591c3c
Gary Woodfine <gary.woodfine@threenine.co.uk>
2025-09-11 19:51:20 +0100

    Extract `SkillsMarquee` into a reusable component and integrate it into the index page.

---
+++ app/components/skills-marquee.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UPageMarquee>
+    <UIcon name="codicon-terminal-linux" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="uil-bitcoin" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="i-simple-icons-litecoin" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="proicons:javascript" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi:language-csharp" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="ant-design:python-outlined" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="devicon-plain:rust" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="fa6-brands:node" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="simple-icons:dotnet" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi:vuejs" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi:nuxt" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="devicon-plain:azure" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi:aws" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="pajamas:terraform" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="mdi:github" class="size-16 shrink-0 text-orange-500" />
+    <UIcon name="codicon:azure-devops" class="size-16 shrink-0 text-orange-500" />
+  </UPageMarquee>
+</template>
+
+<style scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import SkillsMarquee from "~/components/skills-marquee.vue";
+
 const {  global } = useAppConfig()
 const { data: page } = await useAsyncData('index', () => {
   return queryCollection('pages').first()
@@ -110,24 +112,7 @@ useSeoMeta({
       <resume-experience :page />
     </UPageSection>
   </UPage>
-  <UPageMarquee>
-    <UIcon name="codicon-terminal-linux" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="uil-bitcoin" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="i-simple-icons-litecoin" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="proicons:javascript" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi:language-csharp" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="ant-design:python-outlined" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="devicon-plain:rust" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="fa6-brands:node" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="simple-icons:dotnet" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi:vuejs" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi:nuxt" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="devicon-plain:azure" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi:aws" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="pajamas:terraform" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="mdi:github" class="size-16 shrink-0 text-orange-500" />
-    <UIcon name="codicon:azure-devops" class="size-16 shrink-0 text-orange-500" />
-  </UPageMarquee>
+ <skills-marquee />
 
 </template>
 
