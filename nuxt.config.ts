9404cb53ecd09baa5788ab849733f1cfeb111071
Gary Woodfine <lnb0l9dc@duck.com>
2025-09-18 10:37:21 +0100

    Disable SSR in `nuxt.config.ts`, update SPA fallback redirect in `_redirects`, and optimize `nitro` for prerendering.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
     devtools: {enabled: true},
+    ssr: false,
+    nitro: {
+        prerender: {
+            routes: ['/'] // Add specific routes you want to prerender
+        }
+    },
+
     content: {
         build: {
             markdown: {
